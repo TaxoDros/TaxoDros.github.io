c6c41d7610c080c8c418c849cdd0e7553f55f0a9
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2024-01-24 16:09:15 -0600

    create distribution script for Zenodo

---
+++ bin/dist.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Prepares a dist/ folder with a Zenodo compatible file list. 
+#
+#
+
+dist=dist/$(date --iso-8601)
+
+mkdir -p "${dist}" 
+
+copy-content-based() {
+  find data/ -type f\
+   | awk '{print $1 "\t" $1}'\
+   | sed -E 's/([^\t]+)([a-f0-9]{32,}+)$/\2/g'\
+   | parallel --colsep '\t' cp {1} ${dist}/{2} 
+}
+
+copy-name-based() {
+  ls -1\
+   | grep -E "[A-Z]+"\
+   | parallel cp {1} ${dist}/{1}
+}
+
+copy-content-based
+copy-name-based
+
+echo created distribution at ${dist}
