8b28e8359b5668f596e0d897e69b55b88ea661e9
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2024-02-08 10:18:33 -0600

    add taxodros version specification; related https://github.com/bio-guoda/preston/issues/275

--- bin/taxodros-version.sh
+++ bin/taxodros-version.sh
@@ -4,7 +4,7 @@
 #
 #
 
-TAXODROS_VERSION=hash://md5/b8c3a1e1acdfaf8e0bb3c69d220709d8
+TAXODROS_VERSION=hash://md5/3793ab3633d3e94a40d3ab442b6cff7d
 
 PRESTON_OPTS="--algo md5 --remote https://linker.bio,https://zenodo.org"
 
