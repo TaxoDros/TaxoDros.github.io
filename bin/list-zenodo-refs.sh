a070a5872308cec73da547cf4123c8cd2f73eff0
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2025-03-11 15:31:43 -0500

    add type; add publisher

--- bin/list-zenodo-refs.sh
+++ bin/list-zenodo-refs.sh
@@ -10,5 +10,5 @@ VERSION=$(cat "${DATA_DIR}/HEAD")
 preston cat --data-dir "${DATA_DIR}" ${VERSION}\
  | grep hasVersion\
  | preston cat --data-dir "${DATA_DIR}"\
- | jq '.hits.hits[] | { "authors": (.metadata.creators | map(.name) | join(" | ")), "year": .metadata.publication_date, "title": .metadata.title,  "id": .metadata.alternate_identifiers[0].identifier, "doi": .links.doi}'\
+ | jq '.hits.hits[] | { "authors": (.metadata.creators | map(.name) | join(" | ")), "year": .metadata.publication_date, "title": .metadata.title, "journal": .metadata.journal.title, "publication_type": .metadata.resource_type.title, "id": .metadata.alternate_identifiers[0].identifier, "doi": .links.doi}'\
  | mlr --ijsonl --ocsv cat
