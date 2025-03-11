7fecc7cc8375f6ee0b054b4a0778cecaaa44363a
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2025-03-11 14:23:30 -0500

    use pdf prov version instead

--- bin/track-zenodo-associations.sh
+++ bin/track-zenodo-associations.sh
@@ -5,11 +5,11 @@
 
 SCRIPT_DIR=$(dirname $0)
 DATA_DIR="${SCRIPT_DIR}/../zenodo"
-TAXODROS_VERSION=hash://md5/47cda0f604fec63aa3df1f9a21251481
+TAXODROS_MD5_PDF_VERSION=hash://md5/c6ca8257965cccb206b90b022a9446e8
 
 preston track --data-dir "${DATA_DIR}"\
  --algo md5\
- -f <(preston cat --data-dir "${SCRIPT_DIR}/../data" ${TAXODROS_VERSION}\
+ -f <(preston cat --data-dir "${SCRIPT_DIR}/../data" ${TAXODROS_MD5_PDF_VERSION}\
  | grep -oE "hash://md5/[a-f0-9]{32}"\
  | xargs -I{} echo "https://zenodo.org/api/communities/taxodros/records?q=%22{}%22&l=list&limit=1")
 
