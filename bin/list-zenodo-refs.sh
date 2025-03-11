e462b05aeeb30257055aef8444b51ccb765b2758
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2025-03-11 15:20:49 -0500

    fix list-zenodo-refs script

--- bin/list-zenodo-refs.sh
+++ bin/list-zenodo-refs.sh
@@ -10,6 +10,5 @@ VERSION=$(cat "${DATA_DIR}/HEAD")
 preston cat --data-dir "${DATA_DIR}" ${VERSION}\
  | grep hasVersion\
  | preston cat --data-dir "${DATA_DIR}"\
- | jq '.hits.hits[]\
- | { "authors": (.metadata.creators | map(.name) | join(" | ")), "year": .metadata.publication_date, "title": .metadata.title,  "id": .metadata.alternate_identifiers[0].identifier, "doi": .links.doi}'\
- | mlr --ijsonl --otsv cat
+ | jq '.hits.hits[] | { "authors": (.metadata.creators | map(.name) | join(" | ")), "year": .metadata.publication_date, "title": .metadata.title,  "id": .metadata.alternate_identifiers[0].identifier, "doi": .links.doi}'\
+ | mlr --ijsonl --ocsv cat
