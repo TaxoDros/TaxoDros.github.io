2196b24d73ae0791eab1164f46cee5141450b698
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2025-03-12 11:18:09 -0500

    tuning the reference list

--- bin/list-zenodo-refs.sh
+++ bin/list-zenodo-refs.sh
@@ -5,10 +5,10 @@
 
 SCRIPT_DIR=$(dirname $0)
 DATA_DIR="${SCRIPT_DIR}/../zenodo"
-VERSION=$(cat "${DATA_DIR}/HEAD")
+export VERSION=$(cat "${DATA_DIR}/HEAD")
 
 preston cat --data-dir "${DATA_DIR}" ${VERSION}\
  | grep hasVersion\
  | preston cat --data-dir "${DATA_DIR}"\
- | jq '.hits.hits[] | { "authors": (.metadata.creators | map(.name) | join(" | ")), "year": .metadata.publication_date, "title": .metadata.title, "journal": .metadata.journal.title, "publication_type": .metadata.resource_type.title, "id": .metadata.alternate_identifiers[0].identifier, "doi": .links.doi}'\
+ | jq '.hits.hits[] | select(.metadata.alternate_identifiers[0].identifier != null) | { "authors": (.metadata.creators | map(.name) | join(" | ")), "year": .metadata.publication_date, "title": .metadata.title, "journal": (.metadata.journal.title // ""), "publication_type": (.metadata.resource_type.title // ""), "volume": (.metadata.journal.volume // ""),"issue": (.metadata.journal.issue // ""), "pages": (.metadata.journal.pages // ""), "doi": (.links.doi // ""), "id": .metadata.alternate_identifiers[0].identifier, "contentId": .metadata.alternate_identifiers[1].identifier, "zenodoResponseCorpusId": env.VERSION}'\
  | mlr --ijsonl --ocsv cat
