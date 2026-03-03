17cbb5c60331739049e38347cd28eddc78787497
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2026-03-03 14:16:36 -0600

    fix tracking script

--- bin/track-zenodo-associations.sh
+++ bin/track-zenodo-associations.sh
@@ -12,7 +12,7 @@ preston track --data-dir "${DATA_DIR}"\
  -f <(preston ls --algo md5 --data-dir "${SCRIPT_DIR}/../data" --anchor ${TAXODROS_MD5_PDF_VERSION}\
  | grep hasVersion \
  | grep -iE "[.]PDF>" \
- | grep -Eo "hash://md5/[a-f0-9]{32}"
+ | grep -Eo "hash://md5/[a-f0-9]{32}" \
  | xargs -I{} echo "https://zenodo.org/api/communities/taxodros/records?q=%22{}%22&l=list&limit=1"
 )
 
