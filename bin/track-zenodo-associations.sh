5ce2b5cfb6658d6d5ea58dbf2201ad5571724cc9
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2026-03-03 14:13:51 -0600

    track all known taxodros pdf signatures available in Zenodo

--- bin/track-zenodo-associations.sh
+++ bin/track-zenodo-associations.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 # 
-# tracks zenodo query associated with known, versioned taxodros content
+# tracks zenodo query associated with known, versioned taxodros pdf content 
 #
 
 SCRIPT_DIR=$(dirname $0)
 DATA_DIR="${SCRIPT_DIR}/../zenodo"
-TAXODROS_MD5_PDF_VERSION=hash://md5/c6ca8257965cccb206b90b022a9446e8
+source "${SCRIPT_DIR}/version.sh"
 
 preston track --data-dir "${DATA_DIR}"\
  --algo md5\
- -f <(preston cat --data-dir "${SCRIPT_DIR}/../data" ${TAXODROS_MD5_PDF_VERSION}\
- | grep -oE "hash://md5/[a-f0-9]{32}"\
- | xargs -I{} echo "https://zenodo.org/api/communities/taxodros/records?q=%22{}%22&l=list&limit=1")
+ -f <(preston ls --algo md5 --data-dir "${SCRIPT_DIR}/../data" --anchor ${TAXODROS_MD5_PDF_VERSION}\
+ | grep hasVersion \
+ | grep -iE "[.]PDF>" \
+ | grep -Eo "hash://md5/[a-f0-9]{32}"
+ | xargs -I{} echo "https://zenodo.org/api/communities/taxodros/records?q=%22{}%22&l=list&limit=1"
+)
 
 preston head --data-dir "${DATA_DIR}" --algo md5\
  > "${DATA_DIR}/HEAD"
